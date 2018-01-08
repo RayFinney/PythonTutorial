d9f9a28f1d66b96d88968baac8508a026afa071e
kevin <kevinbintzik@gmail.com>
2018-01-08 14:57:36 +0100

    - add some functions to Functions.py

---
+++ Functions.py
@@ -0,0 +1,13 @@
+def my_function():
+    print("My first function in python \o/")
+
+def my_function_with_args(username, lastname):
+    print("Hallo %s %s, My second python function \o/" % (username, lastname))
+
+def calculation_add(number1, number2):
+    return number1 + number2
+
+
+my_function()
+my_function_with_args("Kevin", "Bintzik")
+print(calculation_add(1, 2))
